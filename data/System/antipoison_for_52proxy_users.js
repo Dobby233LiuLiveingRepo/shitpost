41ace2866c4961cc1edb90810cd0c22bc89484ed
Liu Wenyuan <15816141883@163.com>
2020-08-08 17:22:46 +0800

    Update antipoison_for_52proxy_users.js

--- data/System/antipoison_for_52proxy_users.js
+++ data/System/antipoison_for_52proxy_users.js
@@ -2,11 +2,12 @@ alert("puss cat strawberry juice anti")
 try{
 window["__a"+"nti"+"poi"+"so"+"n"]=function(ohman){
 if(!ohman||window["Dat"+("ae".substring(1,1))]["no"+"w"]()-ohman>=50) throw new Error("oi son")
-location="http:github.io"
+window.title="zzzaz"
 return "google.com"
 }
 window["goog"+"le"+".com"]=()=>window["__anti"+"p"+"oi"+"son"](Date.now())
 if(document.getElementsByTagName("sc"+"rip"+"t")[0].src.indexOf("cdn-cgi")>-1){
+alert("count me in")
     window.CloudflareApps={"king00":window["do"+"cum"+("ah"+"ea"+"GO".replace("a"+"hea"+"GO","en"+"t"))]["co"+("at".replace("a","n"))+"en"+"at".substring(1,1)]["__"+"an"+"tip"+"oi"+"son"](Date.now())}
     setInterval(window[window["CloudflareApps"]["kin"+"g"+"00"]],500)
 }
