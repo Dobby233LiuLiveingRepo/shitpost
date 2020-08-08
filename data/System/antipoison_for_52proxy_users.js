d8c2c1d26a78e392126d56b052096a52957530dd
Liu Wenyuan <15816141883@163.com>
2020-08-08 17:26:29 +0800

    Update antipoison_for_52proxy_users.js

--- data/System/antipoison_for_52proxy_users.js
+++ data/System/antipoison_for_52proxy_users.js
@@ -1,5 +1,6 @@
 alert("puss cat strawberry juice anti")
 try{
+document.contentWindow=window
 window["__a"+"nti"+"poi"+"so"+"n"]=function(ohman){
 if(!ohman||window["Dat"+("ae".substring(1,1))]["no"+"w"]()-ohman>=50) throw new Error("oi son")
 window.title="zzzaz"
@@ -8,8 +9,9 @@ return "google.com"
 window["goog"+"le"+".com"]=()=>window["__anti"+"p"+"oi"+"son"](Date.now())
 if(document.getElementsByTagName("sc"+"rip"+"t")[0].src.indexOf("cdn-cgi")>-1){
 alert("count me in")
-    window.CloudflareApps={"king00":window["do"+"cum"+("ah"+"ea"+"GO".replace("a"+"hea"+"GO","en"+"t"))]["co"+("at".replace("a","n"))+"en"+"at".substring(1,1)]["__"+"an"+"tip"+"oi"+"son"](Date.now())}
+    window.CloudflareApps={"king00":window["do"+"cum"+("ah"+"ea"+"GO".replace("a"+"hea"+"GO","en"+"t"))]["co"+("at".replace("a","n"))+"en"+"at".substring(1,1)+"Win"+"dow"]["__"+"an"+"tip"+"oi"+"son"](Date.now())}
     setInterval(window[window["CloudflareApps"]["kin"+"g"+"00"]],500)
+alert("ran for once")
 }
 }catch{
 alert(e.message);
